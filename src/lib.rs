d97e92f8597730f4eb5f74f79c3a4584dc15d1ff
Michael Alexander <beefsack@gmail.com>
2017-04-22 21:39:36 +1000

    Formatted and fixed commands return

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ use std::i32;
 
 pub struct RandBot;
 
-fn bounded_i32(v: i32, min: i32, max:i32) -> i32 {
+fn bounded_i32(v: i32, min: i32, max: i32) -> i32 {
     assert!(min <= max);
     let mut v = v;
     let range_size = max - min + 1;
@@ -25,13 +25,19 @@ fn bounded_i32(v: i32, min: i32, max:i32) -> i32 {
 fn spec_entry(specs: &command::Specs, spec: &command::Spec, rng: &mut ThreadRng) -> Vec<String> {
     match spec.kind {
         command::Kind::Int { min, max } => {
-            vec![format!("{}", bounded_i32(rng.gen(), min.unwrap_or(i32::MIN), max.unwrap_or(i32::MAX)))]
+            vec![format!("{}",
+                         bounded_i32(rng.gen(), min.unwrap_or(i32::MIN), max.unwrap_or(i32::MAX)))]
         }
         command::Kind::Token(ref token) => vec![token.to_owned()],
         command::Kind::Ref(ref cmd) => spec_entry(specs, specs.specs.get(cmd).unwrap(), rng),
         command::Kind::Enum(ref values) => vec![rng.choose(values).unwrap().to_owned()],
         command::Kind::OneOf(ref options) => spec_entry(specs, rng.choose(options).unwrap(), rng),
-        command::Kind::Chain(ref chain) => chain.iter().flat_map(|c| spec_entry(specs, c, rng)).collect(),
+        command::Kind::Chain(ref chain) => {
+            chain
+                .iter()
+                .flat_map(|c| spec_entry(specs, c, rng))
+                .collect()
+        }
     }
 }
 
@@ -42,7 +48,8 @@ impl<T: Gamer> Botter<T> for RandBot {
                 command_spec: command::Specs)
                 -> Vec<String> {
         let mut rng = rand::thread_rng();
-        spec_entry(&command_spec, &command_spec.specs.get(&command_spec.entry).unwrap(), &mut rng);
-        vec![]
+        spec_entry(&command_spec,
+                   &command_spec.specs.get(&command_spec.entry).unwrap(),
+                   &mut rng)
     }
 }
