169c7f6da750f20650a0dfa3e0445e32ced274b6
Michael Alexander <beefsack@gmail.com>
2017-04-23 00:01:01 +1000

    Implemented simplified CLI for RandBot

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
 extern crate rand;
+extern crate serde_json;
 
 extern crate brdgme_game;
+extern crate brdgme_cmd;
 
 use rand::{Rng, ThreadRng};
 
 use brdgme_game::Gamer;
 use brdgme_game::bot::Botter;
 use brdgme_game::command;
+use brdgme_cmd::bot_cli;
 
 use std::i32;
+use std::io::{Read, Write};
 
 pub struct RandBot;
 
@@ -29,7 +33,7 @@ fn spec_entry(specs: &command::Specs, spec: &command::Spec, rng: &mut ThreadRng)
                          bounded_i32(rng.gen(), min.unwrap_or(i32::MIN), max.unwrap_or(i32::MAX)))]
         }
         command::Kind::Token(ref token) => vec![token.to_owned()],
-        command::Kind::Ref(ref cmd) => spec_entry(specs, specs.specs.get(cmd).unwrap(), rng),
+        command::Kind::Ref(ref cmd) => spec_entry(specs, &specs.specs[cmd], rng),
         command::Kind::Enum(ref values) => vec![rng.choose(values).unwrap().to_owned()],
         command::Kind::OneOf(ref options) => spec_entry(specs, rng.choose(options).unwrap(), rng),
         command::Kind::Chain(ref chain) => {
@@ -41,16 +45,35 @@ fn spec_entry(specs: &command::Specs, spec: &command::Spec, rng: &mut ThreadRng)
     }
 }
 
+fn commands(command_spec: &command::Specs) -> Vec<String> {
+    let mut rng = rand::thread_rng();
+    vec![spec_entry(command_spec,
+                    &command_spec.specs[&command_spec.entry],
+                    &mut rng)
+                 .join(" ")]
+}
+
+// / Most bots just want to use `brdgme_cmd::bot_cli`, however because RandBot
+// doesn't care about game / state, we implement a more simplified version of
+// the CLI here. This allows the bot to be used / with arbitrary games as long
+// as the command spec is generated.
+pub fn cli<I, O>(input: I, output: &mut O)
+    where I: Read,
+          O: Write
+{
+    let request = serde_json::from_reader::<_, bot_cli::Request>(input).unwrap();
+    writeln!(output,
+             "{}",
+             serde_json::to_string(&commands(&request.command_spec)).unwrap())
+            .unwrap();
+}
+
 impl<T: Gamer> Botter<T> for RandBot {
     fn commands(_player: usize,
                 _pub_state: T::PubState,
                 _players: &[String],
                 command_spec: command::Specs)
                 -> Vec<String> {
-        let mut rng = rand::thread_rng();
-        vec![spec_entry(&command_spec,
-                        &command_spec.specs.get(&command_spec.entry).unwrap(),
-                        &mut rng)
-                     .join(" ")]
+        commands(&command_spec)
     }
 }
